8b931aa6876615df1d2541e6b95956e83a81bc8d
Franco <franco.mato.contin@gmail.com>
2021-08-30 23:47:23 -0300

    first

---
+++ CreadorDeCarpetas/MainForm.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace CreadorDeCarpetas
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCrearCarpetas = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxCantidad = new System.Windows.Forms.TextBox();
+            this.buttonCambiarCarpeta = new System.Windows.Forms.Button();
+            this.console = new System.Windows.Forms.RichTextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxNombre.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNombre.ForeColor = System.Drawing.Color.White;
+            this.textBoxNombre.Location = new System.Drawing.Point(11, 431);
+            this.textBoxNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxNombre.MaxLength = 20;
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(315, 30);
+            this.textBoxNombre.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(462, 60);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Creador de carpetas por Franco Mato";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonCrearCarpetas
+            // 
+            this.buttonCrearCarpetas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonCrearCarpetas.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.buttonCrearCarpetas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCrearCarpetas.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCrearCarpetas.Location = new System.Drawing.Point(13, 563);
+            this.buttonCrearCarpetas.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCrearCarpetas.Name = "buttonCrearCarpetas";
+            this.buttonCrearCarpetas.Size = new System.Drawing.Size(537, 46);
+            this.buttonCrearCarpetas.TabIndex = 2;
+            this.buttonCrearCarpetas.Text = "Crear Carpetas";
+            this.buttonCrearCarpetas.UseVisualStyleBackColor = true;
+            this.buttonCrearCarpetas.Click += new System.EventHandler(this.buttonCrearCarpetas_ClickAsync);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(4, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 60);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 397);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(320, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ingrese el nombre de las carpetas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 474);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(379, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ingrese la cantidad de carpetas a crear";
+            // 
+            // textBoxCantidad
+            // 
+            this.textBoxCantidad.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxCantidad.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCantidad.ForeColor = System.Drawing.Color.White;
+            this.textBoxCantidad.Location = new System.Drawing.Point(13, 508);
+            this.textBoxCantidad.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxCantidad.MaxLength = 2;
+            this.textBoxCantidad.Name = "textBoxCantidad";
+            this.textBoxCantidad.Size = new System.Drawing.Size(313, 30);
+            this.textBoxCantidad.TabIndex = 1;
+            this.textBoxCantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxCantidad.TextChanged += new System.EventHandler(this.textBoxCantidad_TextChanged);
+            this.textBoxCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxCantidad_KeyPress);
+            // 
+            // buttonCambiarCarpeta
+            // 
+            this.buttonCambiarCarpeta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonCambiarCarpeta.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.buttonCambiarCarpeta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCambiarCarpeta.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCambiarCarpeta.Location = new System.Drawing.Point(333, 328);
+            this.buttonCambiarCarpeta.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCambiarCarpeta.Name = "buttonCambiarCarpeta";
+            this.buttonCambiarCarpeta.Size = new System.Drawing.Size(217, 46);
+            this.buttonCambiarCarpeta.TabIndex = 9;
+            this.buttonCambiarCarpeta.Text = "Cambiar Directorio";
+            this.buttonCambiarCarpeta.UseVisualStyleBackColor = true;
+            this.buttonCambiarCarpeta.Click += new System.EventHandler(this.buttonCambiarCarpeta_Click);
+            // 
+            // console
+            // 
+            this.console.BackColor = System.Drawing.Color.Black;
+            this.console.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.console.Font = new System.Drawing.Font("Century Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.console.ForeColor = System.Drawing.Color.Yellow;
+            this.console.Location = new System.Drawing.Point(9, 70);
+            this.console.Name = "console";
+            this.console.ReadOnly = true;
+            this.console.Size = new System.Drawing.Size(543, 253);
+            this.console.TabIndex = 10;
+            this.console.Text = "";
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.Color.Black;
+            this.progressBar.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.progressBar.Location = new System.Drawing.Point(13, 329);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(313, 45);
+            this.progressBar.TabIndex = 11;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(561, 620);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.buttonCambiarCarpeta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxCantidad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonCrearCarpetas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNombre);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Creador de Carpetas Por Franco28";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCrearCarpetas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxCantidad;
+        private System.Windows.Forms.Button buttonCambiarCarpeta;
+        private System.Windows.Forms.RichTextBox console;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ CreadorDeCarpetas/MainForm.cs
@@ -0,0 +1,187 @@
+﻿
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CreadorDeCarpetas
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private string exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+        public void consoleA(string message)
+        {
+            this.Invoke((Action)delegate
+            {
+                console.AppendText("\n" + message);
+                console.ScrollToCaret();
+            });
+        }
+
+        public void CreateFileWatcher()
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = exePath;
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.ini";
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            consoleA("\nEl archivo: {" + e.FullPath + "}  {" + e.ChangeType + "}\n");
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            consoleA("\nEl archivo: {" + e.OldFullPath + "} se renombro a {"+ e.FullPath + "}\n");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            progressBar.Hide();
+            CreateFileWatcher();
+            var MyIni = new IniFile();
+            MyIni.Write("FolderPath", exePath);
+            consoleA("\nDirectorio Actual: " + MyIni.Read("FolderPath") + "\n");
+        }
+
+        private void buttonCrearCarpetas_ClickAsync(object sender, EventArgs e)
+        {
+            if (textBoxNombre.Text == "")
+            {
+                MessageBox.Show("El casillero Nombre De Las Carpetas no puede estar vacio!", "Casillero Nombre De Las Carpetas vacio!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (textBoxCantidad.Text == "")
+            {
+                MessageBox.Show("El casillero Cantidad De Las Carpetas no puede estar vacio!", "Casillero Cantidad De Las Carpetas vacio!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int cantidadCarpetas;
+            string nombreCarpetas;
+
+            try
+            {
+                 cantidadCarpetas = int.Parse(textBoxCantidad.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Solo se admiten numeros enteros!", "Error al leer el numero ingresado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (cantidadCarpetas > 20)
+            {
+                DialogResult dialogResult = MessageBox.Show("Ingreso un total de: " + cantidadCarpetas + " carpetas y esto puede ser MUY PELIGROSO si su PC tiene poca memoria RAM y memoria ROM. Desea continuar?", "CUIDADO! PELIGRO!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                
+                if (dialogResult == DialogResult.No)
+                    return;
+            }
+
+            console.Clear();
+
+            consoleA("\nParchando textos y numeros...");
+
+            nombreCarpetas = textBoxNombre.Text;
+            cantidadCarpetas = int.Parse(textBoxCantidad.Text) + 1;
+
+            consoleA("\nParchando textos y numeros... {OK}");
+
+            progressBar.Show();
+            progressBar.Maximum = cantidadCarpetas;
+
+            for (var i = 0; i < cantidadCarpetas; i++)
+            {
+                progressBar.Value = i;
+                consoleA("\nCreando carpeta: {" + nombreCarpetas + "}");
+                EvaluatePath(nombreCarpetas + i.ToString());
+                console.Clear();
+                progressBar.Hide();
+            }
+
+            var MyIni = new IniFile();
+            consoleA("\nSe crearon {" + cantidadCarpetas + "} carpetas con exito en {" + MyIni.Read("FolderPath") + "}");
+        }
+
+        private String EvaluatePath(String path)
+        {
+            try
+            {
+                var MyIni = new IniFile();
+                String folder = Path.GetDirectoryName(path);
+
+                if (!Directory.Exists(folder))
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(MyIni.Read("FolderPath") + @"\" + path);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("ERROR: " + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return "";
+            }
+            return path;
+        }
+
+        private void buttonCambiarCarpeta_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+                folderDlg.ShowNewFolderButton = true;
+
+                DialogResult result = folderDlg.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    var MyIni = new IniFile();
+                    MyIni.Write("FolderPath", folderDlg.SelectedPath);
+                    console.Clear();
+                    consoleA("Directorio Actual: " + MyIni.Read("FolderPath"));
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("ERROR: " + ex, "Error al abrir directorio", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void textBoxCantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;         
+        }
+
+        private void textBoxCantidad_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxCantidad.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                int cantidadCarpetas = int.Parse(textBoxCantidad.Text);
+                if (cantidadCarpetas > 20)
+                    textBoxCantidad.BackColor = Color.Red;
+                else
+                    textBoxCantidad.BackColor = Color.Black;
+            }
+        }
+    }
+}
